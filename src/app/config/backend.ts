a97e147a4daf2072e832db6b33dd2130108bad02
FeelHippo <mezzasegolas@gmail.com>
2025-11-21 16:29:52 +0100

    fix(domain): trying to fic the remote compile error

--- src/app/config/backend.ts
+++ src/app/config/backend.ts
@@ -6,7 +6,7 @@ import { TypeInput } from 'supertokens-node/types';
 
 export const connectionURI =
   process.env.CORE_API_ENDPOINT ??
-  'https://<OBTAIN_THIS_FROM_ADMIN>.aws.supertokens.io';
+  'https://OBTAIN_THIS_FROM_ADMIN.aws.supertokens.io';
 
 export const backendConfig = (): TypeInput => {
   return {
